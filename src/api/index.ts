0f394fdc67607bf0a500d2f0981c9b1d3dc32843
James <james@sqrx.com>
2023-06-11 10:26:22 +0800

    Export directly from API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,27 +1,12 @@
-import * as env from "../env.js";
-import * as boards from "./boards.js";
-import * as categories from "./categories.js";
-import * as changelogEntries from "./changelog-entries.js";
-import * as comments from "./comments.js";
-import * as companies from "./companies.js";
-import * as opportunities from "./opportunities.js";
-import * as posts from "./posts.js";
-import * as statusChanges from "./status-changes.js";
-import * as tags from "./tags.js";
-import * as users from "./users.js";
-import * as votes from "./votes.js";
-
-export const canny = {
-  ...env,
-  ...boards,
-  ...categories,
-  ...changelogEntries,
-  ...comments,
-  ...companies,
-  ...opportunities,
-  ...posts,
-  ...statusChanges,
-  ...tags,
-  ...users,
-  ...votes,
-};
+export * from "../env.js";
+export * from "./boards.js";
+export * from "./categories.js";
+export * from "./changelog-entries.js";
+export * from "./comments.js";
+export * from "./companies.js";
+export * from "./opportunities.js";
+export * from "./posts.js";
+export * from "./status-changes.js";
+export * from "./tags.js";
+export * from "./users.js";
+export * from "./votes.js";
